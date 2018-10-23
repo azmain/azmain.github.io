a1aacf79178914c574cd7016561e510ad2f8f869
Azmain Islam <azmainnishan@gmail.com>
2018-10-24 02:44:09 +0600

    index editing

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,43 +1,49 @@
-$(document).ready(function(){
-	smoothScroll(1200);
 
-	$("contact mail").fitText(1, { minFontSize: '10px', maxFontSize: '65px' });
+$(document).ready(function () {
+  smoothScroll(1200);
+
+  /** Text Size fitting with window size */
+  $("contact mail").fitText(1, { minFontSize: '10px', maxFontSize: '65px' });
 });
 
-function smoothScroll (duration){
-	$('a[href^="#"]').on('click', function(event){
-		var target = $($(this).attr('href'));
-
-		if(target.length){
-			event.preventDefault();
-			$('html, body').animate({
-				scrollTop: target.offset().top - 100
-			},duration);
-		}
-	});
+/**
+ * Smooth scrolling on click menu navigation
+ */
+function smoothScroll(duration) {
+  $('a[href^="#"]').on('click', function (event) {
+    var target = $($(this).attr('href'));
+    let id = $(this).attr("id");
+    $('a[href^="#"]').removeAttr("class");
+    $("#" + id).attr("class", "active");
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top - 100
+      }, duration);
+    }
+  });
 }
 
-//fittext to sizing text according to window size
+/* fit text to sizing text according to window size */
+(function ($) {
 
-(function( $ ){
-
-  $.fn.fitText = function( kompressor, options ) {
+  $.fn.fitText = function (kompressor, options) {
 
     // Setup options
     var compressor = kompressor || 1,
-        settings = $.extend({
-          'minFontSize' : Number.NEGATIVE_INFINITY,
-          'maxFontSize' : Number.POSITIVE_INFINITY
-        }, options);
+      settings = $.extend({
+        'minFontSize': Number.NEGATIVE_INFINITY,
+        'maxFontSize': Number.POSITIVE_INFINITY
+      }, options);
 
-    return this.each(function(){
+    return this.each(function () {
 
       // Store the object
       var $this = $(this);
 
       // Resizer() resizes items based on the object width divided by the compressor * 10
       var resizer = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor * 10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
       };
 
       // Call once to set.
@@ -50,4 +56,12 @@ function smoothScroll (duration){
 
   };
 
-})( jQuery );
+})(jQuery);
+
+
+/* $(window).scroll(function () {
+
+  console.log("hello");
+  console.log($(this).closest("section"));
+  $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
+}); */
